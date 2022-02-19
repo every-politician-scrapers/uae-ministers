86c0172d3de37a1214287b0618d4468d1518a1a0
Tony Bowden <tony@tmtm.com>
2022-02-19 19:01:02 +0200

    compare enwiki Abu Dhabi rulers

---
+++ sources/emir-abu-dhabi/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Ruled'
+  end
+
+  def table_number
+    1
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[dates name].freeze
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
