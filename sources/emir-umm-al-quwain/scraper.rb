a35342464a86340aa588c1f824086c2b371df5c1
Tony Bowden <tony@tmtm.com>
2022-02-19 20:27:28 +0200

    compare enwiki rulers of Umm Al Quwain

---
+++ sources/emir-umm-al-quwain/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Ruled'
+  end
+
+  def table_number
+    6
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[dates name].freeze
+    end
+
+    def raw_combo_date
+      super.gsub(/\(.*?\)/, '').tidy
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
